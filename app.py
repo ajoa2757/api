1329882a0f80b35bbaba2390be3505b607178849
ajoa2757 <ajoa4936@gmail.com>
2021-12-23 03:33:00 +0900

    팔언팔 엔드포인트 추가

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
 
 app = Flask(__name__)
+
+#set 자료형이 포함되면 jsonify 형태로 동작을 하지 못하는 문제가 있다
+#이것을 위한 custom 인코더라고 한다.
+class CustomJSONEncoder(JSONEncoder):
+        def default(self, obj):
+                if isinstance(obj, set):
+                        return list(obj)
+                return JSONEncoder.default(self, obj)
+
+
+app.json_encoder = CustomJSONEncoder
+
 
 #사용자들의 정보가 담길 dictionaty.
 #여기에 dictionary 의 dictionary 가 담길 것이다
@@ -11,8 +24,9 @@ app.users = {}
 #제대로된 데이터베이스 시스템을 사용하면서 이 문제는 해결 될 것이다
 app.id_count = 1
 app.tweets = []
-#새로운 수정 사항?
-#그럼 이렇게 바꾸면 또 빨간색 될거야?
+
+#팔로우 관계를 정리할 데이터베이스?
+
 @app.route("/ping", methods = ['GET'])
 def ping():
         return "pong"
@@ -69,6 +83,41 @@ def tweet():
 
         #여기까지 왔을 때의 정상적인 출력
         return '',200
+
+@app.route("/follow", methods=['POST'])
+def follow():
+
+        #마찬가지로 payload 변수에 request 에서 전달된 변수를 담는다
+        #json 은 list 의 형태로 사용자가 제출한 정보를 잘 담는다
+        payload = request.json
+        user_id = int(payload['id'])
+        user_id_to_follow = int(payload['follow'])
+
+        #파이썬은 마치 모든 변수가 참조자인 것 처럼 동작하는 듯 하다?
+        #아래 코드를 통해 user 는 마치 app.users[user_id] 의 참조자처럼 동작한다
+        user = app.users[user_id]
+
+        #setdefault 는 1번째 인자가 key 로서 존재한다면 그 value 를 리턴한다
+        #만약 존재하지 않으면 2번째 인자를 value 로서 생성하고 또 그 value 를 리턴한다
+        #아래 코드로 생성된 빈 set 가 리턴되거나, 이미 존재하는 follow 의 set 이 리턴되는 것이다.
+        user.setdefault('follow',set()).add(user_id_to_follow)
+
+        return jsonify(user)
+
+@app.route("/unfollow", methods=['POST'])
+def unfollow():
+
+        #전반적으로 follow 와 유사한 동작인데 add 대신 discard 한다.
+
+        payload = request.json
+        user_id = int(payload['id'])
+        user_id_to_unfollow = int(payload['unfollow'])
+
+        user = app.users[user_id]
+        user.setdefault('follow',set()).discard(user_id_to_unfollow)
+
+        return jsonify(user)
+
 
 if __name__ =='__main__':
         app.run()
